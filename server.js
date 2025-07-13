a46019e2b63715fc2531d398c13c38d68a35ddbd
cimey <cumakilinc27@gmail.com>
2025-07-13 15:29:02 +0200

    changes

--- server.js
+++ server.js
@@ -141,4 +141,6 @@ function checkDraw(gameState) {
     console.log("check draw", result);
     return result;
 }
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server running on ${port}`));
